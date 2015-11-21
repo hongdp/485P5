bc921026294ebe8e67dcbd7d4e7da407423a2d22
Chengwei Dai <daichengwei@gmail.com>
2015-11-20 23:20:57 -0500

    up to read net file

--- hits/eecs485pa5h.cpp
+++ hits/eecs485pa5h.cpp
@@ -39,14 +39,49 @@ int main(int argc, char const *argv[]) {
     cout << str << " ";
   } cout << endl;
 
-  map<int, PageNode*> id_to_pagenode_map;
+  map<int, PageNode*> Node_map;
   map<string, set<PageNode*> > invert_index_map;
+
+  // read net file
+  string ver_title;
+  int num_ver;
   ifstream in_net_stream(IN_NET_FILE.c_str());
   if (!in_net_stream.is_open()) {
     cout << "Failed to open the net file" << endl;
     exit(1);
   }
-  
+  // read Vertices
+  in_net_stream >> ver_title >> num_ver;
+  if (ver_title != "*Vertices") {
+    cout << "Wrong file formate no *Vertices" << endl;
+  }
+  for (size_t i = 0; i < size_t(num_ver); i++) {
+    int id;
+    string name;
+    in_net_stream >> id >> name;
+    PageNode* tmp_node = new PageNode(id);
+    Node_map[id] = tmp_node;
+  }
+  // read edges
+  string edge_title;
+  int num_edge;
+  in_net_stream >> edge_title >> num_edge;
+  if (edge_title != "*Arcs") {
+    cout << "Wrong file formate no *Arcs" << endl;
+  }
+  for (size_t i = 0; i < size_t(num_edge); i++) {
+    int src, dest;
+    in_net_stream >> src >> dest;
+	  if (src == dest) {
+		  continue;
+	  }
+    PageNode* src_ptr = Node_map[src];
+    PageNode* dest_ptr = Node_map[dest];
+    dest_ptr->setSrc(src_ptr);
+    src_ptr->setDest(dest_ptr);
+  }
+  in_net_stream.close();
+
 
 
 
